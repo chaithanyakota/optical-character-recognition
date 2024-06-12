d4ec628a92ee441fc31f3115d1a78087e13d580c
ChaithanyaKota <chaithanya.kota007@gmail.com>
2024-06-11 23:48:14 -0500

    load dataset

--- traffic.py
+++ traffic.py
@@ -1,6 +1,36 @@
+
 import numpy as np
-from sklearn.utils import shuffle 
-from skimage import exposure
 import matplotlib
 from matplotlib import pyplot
 import tensorflow as tf
+
+from sklearn.utils import shuffle
+from skimage import exposure
+
+import pickle
+
+def load_pickled_data(file, cols): 
+    """
+    Loads pickled training and test data.
+    
+    Parameters
+    ----------
+    file    : Name of the pickle file.
+              
+    columns : list of strings
+              List of columns in pickled data we're interested in.
+
+    Returns
+    -------
+    A tuple of datasets for given columns.    
+    """
+    
+    with open(file, mode='rb') as f: 
+        dataset = pickle.load(f)
+        
+    return tuple(map(lambda c: dataset[c], cols))
+    
+    
+
+
+
