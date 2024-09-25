dce7690ea2173177dd91aae2ca927b3d9694152c
ChaithanyaKota <chaithanya.kota007@gmail.com>
2024-09-24 21:01:55 -0500

    add custom data

---
+++ augment.py
@@ -0,0 +1,95 @@
+import numpy as np
+from skimage.transform import rotate
+from skimage.transform import warp
+from skimage.transform import ProjectiveTransform
+import random
+
+def flip_extend(X, y):
+    # Classes of signs that, when flipped horizontally, should still be classified as the same class
+    self_flippable_horizontally = np.array([11, 12, 13, 15, 17, 18, 22, 26, 30, 35])
+    # Classes of signs that, when flipped vertically, should still be classified as the same class
+    self_flippable_vertically = np.array([1, 5, 12, 15, 17])
+    # Classes of signs that, when flipped horizontally and then vertically, should still be classified as the same class
+    self_flippable_both = np.array([32, 40])
+    # Classes of signs that, when flipped horizontally, would still be meaningful, but should be classified as some other class
+    cross_flippable = np.array([
+        [19, 20], 
+        [33, 34], 
+        [36, 37], 
+        [38, 39],
+        [20, 19], 
+        [34, 33], 
+        [37, 36], 
+        [39, 38],   
+    ])
+    num_classes = 43
+    
+    X_extended = np.empty([0, X.shape[1], X.shape[2], X.shape[3]], dtype = X.dtype)
+    y_extended = np.empty([0], dtype = y.dtype)
+    
+    for c in range(num_classes):
+        # First copy existing data for this class
+        X_extended = np.append(X_extended, X[y == c], axis = 0)
+        # If we can flip images of this class horizontally and they would still belong to said class...
+        if c in self_flippable_horizontally:
+            # ...Copy their flipped versions into extended array.
+            X_extended = np.append(X_extended, X[y == c][:, :, ::-1, :], axis = 0)
+        # If we can flip images of this class horizontally and they would belong to other class...
+        if c in cross_flippable[:, 0]:
+            # ...Copy flipped images of that other class to the extended array.
+            flip_class = cross_flippable[cross_flippable[:, 0] == c][0][1]
+            X_extended = np.append(X_extended, X[y == flip_class][:, :, ::-1, :], axis = 0)
+        # Fill labels for added images set to current class.
+        y_extended = np.append(y_extended, np.full((X_extended.shape[0] - y_extended.shape[0]), c, dtype = int))
+        
+        # If we can flip images of this class vertically and they would still belong to said class...
+        if c in self_flippable_vertically:
+            # ...Copy their flipped versions into extended array.
+            X_extended = np.append(X_extended, X_extended[y_extended == c][:, ::-1, :, :], axis = 0)
+        # Fill labels for added images set to current class.
+        y_extended = np.append(y_extended, np.full((X_extended.shape[0] - y_extended.shape[0]), c, dtype = int))
+        
+        # If we can flip images of this class horizontally AND vertically and they would still belong to said class...
+        if c in self_flippable_both:
+            # ...Copy their flipped versions into extended array.
+            X_extended = np.append(X_extended, X_extended[y_extended == c][:, ::-1, ::-1, :], axis = 0)
+        # Fill labels for added images set to current class.
+        y_extended = np.append(y_extended, np.full((X_extended.shape[0] - y_extended.shape[0]), c, dtype = int))
+    
+    return (X_extended, y_extended)
+
+
+def rotate(X, intensity):
+    for i in range(X.shape[0]):
+        delta = 30. * intensity # scale using augmentation intensity
+        X[i] = rotate(X[i], random.uniform(-delta, delta), mode = 'edge')
+    return X  
+
+def apply_projection_transform(X, intensity):
+    image_size = X.shape[1]
+    d = image_size * 0.3 * intensity
+    for i in range(X.shape[0]):
+        tl_top = random.uniform(-d, d)     # Top left corner, top margin
+        tl_left = random.uniform(-d, d)    # Top left corner, left margin
+        bl_bottom = random.uniform(-d, d)  # Bottom left corner, bottom margin
+        bl_left = random.uniform(-d, d)    # Bottom left corner, left margin
+        tr_top = random.uniform(-d, d)     # Top right corner, top margin
+        tr_right = random.uniform(-d, d)   # Top right corner, right margin
+        br_bottom = random.uniform(-d, d)  # Bottom right corner, bottom margin
+        br_right = random.uniform(-d, d)   # Bottom right corner, right margin
+
+        transform = ProjectiveTransform()
+        transform.estimate(np.array((
+                (tl_left, tl_top),
+                (bl_left, image_size - bl_bottom),
+                (image_size - br_right, image_size - br_bottom),
+                (image_size - tr_right, tr_top)
+            )), np.array((
+                (0, 0),
+                (0, image_size),
+                (image_size, image_size),
+                (image_size, 0)
+            )))
+        X[i] = warp(X[i], transform, output_shape=(image_size, image_size), order = 1, mode = 'edge')
+
+    return X
